667fb3be02ad1b5e7d21fee2cf257ea9ba39539b
hae-on <solbi2004@naver.com>
2022-06-23 18:16:44 +0900

    [Fix]git 충돌 부분 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global-style';
 import Nav from './components/Organisms/Nav';
-import ThumbnailList from './components/Molecule/ThumbnailList';
 import Footer from './components/Organisms/Footer';
 import Router from './Router';
-
 
 const App: React.FC = () => {
   return (
@@ -14,20 +11,8 @@ const App: React.FC = () => {
       <GlobalStyle />
       <div className="App">
         <Nav />
-        <HomeTemplateDiv>
-          <MenuPart>
-            <Menu />
-          </MenuPart>
-          <MainPart>
-            <Routes>
-              <Route path="/" element={<></>} />
-              <Route path="/:category" element={<ThumbnailList />} />
-            </Routes>
-          </MainPart>
-        </HomeTemplateDiv>
         <Router />
         <Footer />
-
       </div>
     </BrowserRouter>
   );
