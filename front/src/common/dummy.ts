09375b15ed3de66a6eea49fd97cb7eb2807d549c
ksone02 <eksovus@gmail.com>
2022-06-26 14:59:56 +0900

    [Feat] 더미 데이터 생성

--- front/src/common/dummy.ts
+++ front/src/common/dummy.ts
@@ -1,42 +1,7 @@
-export const anotherAssetData = [
-  {
-    id: 1,
-    img: 'https://post-phinf.pstatic.net/MjAyMDAxMDNfMjQy/MDAxNTc4MDQ0MjQ5ODk2.YMQlePTP_1h2cN1hGjklsvksCKSmohu_pWvAWuaTeYog.9bJ7G5l6Fw5kebk8JAvFl-PNmoV12Fnkn5Swp5E8PhMg.JPEG/%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C_%2816%29.jpeg?type=w1200',
-  },
-  {
-    id: 2,
-    img: 'https://techrecipe.co.kr/wp-content/uploads/2022/06/220602_Starry-Night-in-Minecraft_00001.png',
-  },
-  {
-    id: 3,
-    img: 'https://www.designdb.com/usr/upload/editor/email/202108201601108bd01a8a-7766-4349-a923-e061eb591391.jpg',
-  },
-  {
-    id: 4,
-    img: 'https://i.ytimg.com/vi/J41rnEDa9SQ/maxresdefault.jpg',
-  },
-  {
-    id: 5,
-    img: 'https://blog.kakaocdn.net/dn/bC4QLQ/btrqbFcKOnS/Es0tKOlkQvdRAU1q0sKtI1/img.png',
-  },
-  {
-    id: 6,
-    img: 'https://cdn.gamemeca.com/data_center/222/706/20201209144458.jpg',
-  },
-  {
-    id: 7,
-    img: 'https://post-phinf.pstatic.net/MjAyMDAxMjlfMTM3/MDAxNTgwMzA2OTQ2Nzcx.SNPF5TiRz-LUIy7AIH0L98kcV9yox3J52g1kiQklHucg.QJ4TELFzpQVD-NfE_1x0KlgqxF1Zr-OlwD9wHrxAR1gg.JPEG/114.jpg?type=w1200',
-  },
-  {
-    id: 8,
-    img: 'https://cdn.imweb.me/thumbnail/20200315/5b3e7a075d76f.jpg',
-  },
-];
-
 export const Arts = [
   {
-    ArtsId: 1,
-    ArtsCategoryName: '대규모 마인크래프트 맵',
+    ArtsId: 0,
+    ArtsCategoryName: '번들 및 중규모 마인크래프트 맵',
     Author: 'GBF Studio',
     ArtsName: '반 고흐 해바라기',
     ArtsTime: '21. 05. 30',
@@ -49,8 +14,8 @@ export const Arts = [
     DownloadUrl: '/',
   },
   {
-    ArtsId: 2,
-    ArtsCategoryName: '대규모 마인크래프트 맵',
+    ArtsId: 1,
+    ArtsCategoryName: '번들 및 중규모 마인크래프트 맵',
     Author: 'KSW Studio',
     ArtsName: '크라이슬러 빌딩',
     ArtsTime: '22. 06. 23',
@@ -59,6 +24,295 @@ export const Arts = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
     ArtsDetailImage:
       'http://appdata.hungryapp.co.kr/data_file/data_img_m/201211/cLgXaXSe9rSOJBVF8JSx3HgE1352627247.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 2,
+    ArtsCategoryName: '배경음악',
+    Author: 'GBF Studio',
+    ArtsName: 'GANADARA (Feat. 아이유)',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://s3-alpha-sig.figma.com/img/2b60/6e42/addccf350a28193868c918e2f39945e1?Expires=1656892800&Signature=V9TA8bje3aKcZpmcg-zTKdJe-dQRxuyE1SMlNHCIUc~9xeyxbZZS4za-sG-DpX3yHjHHzQtcImqXYQDWM1H~nWov5~6BvYsHlZjrDzf0IV8nuRktc3lpnbvGrpOAH3l66vZg3zNzg0Kg8sjYH5-PgIhGJEjwvl7J7MvMFztczw~UoZm7o~4mlLIvmHJUde5Q1rW6UhvP7uPVHSFs1lRRohlBdv~Y1ho7MzV9Kj2ABs6gl2e6eMX8VytYgkta7HjbWwJ2CT34TqpxGMDhnNtuvaWB9-JyvMgNVOo8LZdpVusuN1bUggYFiEiNRk8SGqVednAeOAT7PRH63lC7EAqetA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 3,
+    ArtsCategoryName: '대규모 마인크래프트 맵',
+    Author: 'GBF Studio',
+    ArtsName: '대규모 중세 맵',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://post-phinf.pstatic.net/MjAyMDAxMDNfMjQy/MDAxNTc4MDQ0MjQ5ODk2.YMQlePTP_1h2cN1hGjklsvksCKSmohu_pWvAWuaTeYog.9bJ7G5l6Fw5kebk8JAvFl-PNmoV12Fnkn5Swp5E8PhMg.JPEG/%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C_%2816%29.jpeg?type=w1200',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 4,
+    ArtsCategoryName: '대규모 마인크래프트 맵',
+    Author: 'GBF Studio',
+    ArtsName: '반 고흐의 별이 빛나는 밤',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://techrecipe.co.kr/wp-content/uploads/2022/06/220602_Starry-Night-in-Minecraft_00001.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 5,
+    ArtsCategoryName: '대규모 마인크래프트 맵',
+    Author: 'GBF Studio',
+    ArtsName: '대도서관',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://www.designdb.com/usr/upload/editor/email/202108201601108bd01a8a-7766-4349-a923-e061eb591391.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 6,
+    ArtsCategoryName: '번들 및 중규모 마인크래프트 맵',
+    Author: 'GBF Studio',
+    ArtsName: '에펠탑',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage: 'https://i.ytimg.com/vi/J41rnEDa9SQ/maxresdefault.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 7,
+    ArtsCategoryName: '번들 및 중규모 마인크래프트 맵',
+    Author: 'GBF Studio',
+    ArtsName: '메이플스토리 시간의 신전',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://blog.kakaocdn.net/dn/bC4QLQ/btrqbFcKOnS/Es0tKOlkQvdRAU1q0sKtI1/img.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 8,
+    ArtsCategoryName: '더 샌드박스 에셋',
+    Author: 'GBF Studio',
+    ArtsName: '콜로세움',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://cdn.gamemeca.com/data_center/222/706/20201209144458.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 9,
+    ArtsCategoryName: '더 샌드박스 에셋',
+    Author: 'GBF Studio',
+    ArtsName: '중국풍 사원',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://post-phinf.pstatic.net/MjAyMDAxMjlfMTM3/MDAxNTgwMzA2OTQ2Nzcx.SNPF5TiRz-LUIy7AIH0L98kcV9yox3J52g1kiQklHucg.QJ4TELFzpQVD-NfE_1x0KlgqxF1Zr-OlwD9wHrxAR1gg.JPEG/114.jpg?type=w1200',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 10,
+    ArtsCategoryName: 'VIP 라운지',
+    Author: 'GBF Studio',
+    ArtsName: '마인크래프트 섬',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://cdn.imweb.me/thumbnail/20200315/5b3e7a075d76f.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 11,
+    ArtsCategoryName: 'VIP 라운지',
+    Author: 'GBF Studio',
+    ArtsName: '긍정전 광화문',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://t1.daumcdn.net/thumb/R1000x0/?fname=https://i.imgur.com/L75KYjY.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 12,
+    ArtsCategoryName: '3D모델링',
+    Author: 'GBF Studio',
+    ArtsName: '3D 여성 캐릭터',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage: 'https://pbs.twimg.com/media/E9Jn1ppVUAMsXyV.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 13,
+    ArtsCategoryName: '3D모델링',
+    Author: 'GBF Studio',
+    ArtsName: '3D 우물',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://static.wixstatic.com/media/ed148c_dc5d638a592b4a67b5a2c2309421320e~mv2.png/v1/fill/w_320,h_266,q_90/ed148c_dc5d638a592b4a67b5a2c2309421320e~mv2.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 14,
+    ArtsCategoryName: '블록벤처 모델링',
+    Author: 'GBF Studio',
+    ArtsName: '거미',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage: 'https://pbs.twimg.com/media/FKpK_RSUYAcs3hG.jpg:large',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 15,
+    ArtsCategoryName: '블록벤처 모델링',
+    Author: 'GBF Studio',
+    ArtsName: '드레드',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage: 'https://i.imgur.com/n7f3XuU.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 16,
+    ArtsCategoryName: '복쉘 아트워크 obj',
+    Author: 'GBF Studio',
+    ArtsName: '초록 집',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://i.pinimg.com/originals/b0/1a/0e/b01a0eabdbb2679775c0b2aab3a1a8f1.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 17,
+    ArtsCategoryName: '복쉘 아트워크 obj',
+    Author: 'GBF Studio',
+    ArtsName: '폐허가 된 도시',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage: 'https://pbs.twimg.com/media/EurzcL8WQAYalLW.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 18,
+    ArtsCategoryName: '폰트',
+    Author: 'GBF Studio',
+    ArtsName: 'Rategin Font',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://i0.wp.com/www.dafontfree.co/wp-content/uploads/2021/07/Rategin-Bold-Script-Font-1.jpg?fit=2340%2C1560&ssl=1',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 19,
+    ArtsCategoryName: '폰트',
+    Author: 'GBF Studio',
+    ArtsName: '프리텐다드',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FQh1ub%2Fbtriq776rrZ%2FSR4ptRnlzEzNF1p2oHakY0%2Fimg.png',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 20,
+    ArtsCategoryName: '음악',
+    Author: 'GBF Studio',
+    ArtsName: '이상주의 - Colde',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://img.hankyung.com/photo/202101/01.25017855.1-1200x.jpg',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 21,
+    ArtsCategoryName: '음악',
+    Author: 'GBF Studio',
+    ArtsName: '12 - Beenzino',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://ww.namu.la/s/a598f881925e8a3cc4228b17b101266c2e839ec7f5c9c130273d297fbcbf51d598128a1dd995a6a90383d0df09e0b9583cdbe6f23812dd8e6f9fe46b767ccfe851f31a820e111a4547fc4b86fc5a619e2c35626d96d29e8e3881bc03160e6260',
+    CountDownload: 7368,
+    DownloadUrl: '/',
+  },
+  {
+    ArtsId: 22,
+    ArtsCategoryName: '배경음악',
+    Author: 'GBF Studio',
+    ArtsName: 'Stories - Avicii',
+    ArtsTime: '21. 05. 30',
+    ArtsViews: 16866,
+    ArtsDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum ultrices montes, phasellus. Rutrum massa pharetra nunc suspendisse augue. Nam feugiat in tempus, ut accumsan non. At donec eget aenean dui volutpat sed varius elementum.',
+    ArtsDetailImage:
+      'https://i.discogs.com/5QNvKoTkOa0YQ2b2W91Is9DZ5oAnTmQ2PESkP4-0thE/rs:fit/g:sm/q:90/h:590/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIyMDAz/NTkxLTE2NDM4NjUz/NjktNjIwOC5qcGVn.jpeg',
     CountDownload: 7368,
     DownloadUrl: '/',
   },
